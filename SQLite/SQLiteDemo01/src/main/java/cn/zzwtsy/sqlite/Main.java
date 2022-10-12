e6f5cd341296d3a7258ebd82002bdea96fab9de9
zzwtsy <m3226257319@gmail.com>
2022-10-12 10:51:53 +0800

    Update SQL

--- SQLite/SQLiteDemo01/src/main/java/cn/zzwtsy/sqlite/Main.java
+++ SQLite/SQLiteDemo01/src/main/java/cn/zzwtsy/sqlite/Main.java
@@ -1,51 +1,59 @@
 package cn.zzwtsy.sqlite;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 /**
  * @author zzwtsy
  */
+@Slf4j
 public class Main {
-    public static void main(String[] args) {
-        Connection connection;
-        Statement statement;
-        File file = new File("test.db");
+    static final String FILE_NAME = "test.db";
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        SqliteHelper sqliteHelper = new SqliteHelper(FILE_NAME);
+        File file = new File(FILE_NAME);
         if (!file.exists()) {
-            connection = connectDatabase();
+            sqliteHelper.getConnection(FILE_NAME);
             try {
-                statement = connection.createStatement();
-                String sql = "CREATE TABLE T_userSettings"
-                        + "(NAME TEXT NOT NULL,"
-                        + "PASSWORD VARCHAR(16),"
-                        + "THEME CHAR(6)"
-                        + ")";
-                statement.execute(sql);
-                statement.close();
+                String sql = "CREATE TABLE T_userSettings (NAME CHAR(10) PRIMARY KEY NOT NULL,PASSWORD VARCHAR(16),THEME CHAR(6))";
+                sqliteHelper.executeQuery(sql);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println("表创建成功");
-        }else {
-            connectDatabase();
+            log.info("表创建成功");
+        } else {
+            try {
+                sqliteHelper.getConnection(FILE_NAME);
+            } catch (ClassNotFoundException | SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
-    }
-
-    /**
-     * 连接数据库
-     */
-    public static Connection connectDatabase(){
-        Connection connection;
+        String userName, password;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入用户名:");
+        userName = scanner.next();
+        System.out.print("请输入密码:");
+        password = scanner.next();
         try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
-        } catch (ClassNotFoundException | SQLException e) {
+            String insertStatement = "INSERT INTO T_userSettings(NAME, PASSWORD, THEME) VALUES("
+                    + userName
+                    + "," + password
+                    + ",'DARK')";
+            sqliteHelper.executeUpdate(insertStatement);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("加载数据库成功");
-        return connection;
+        try {
+            ResultSet resultSet = sqliteHelper.executeQuery("SELECT * FROM T_userSettings");
+            String name = resultSet.getString("NAME");
+            String pwd = resultSet.getString("PASSWORD");
+            System.out.println(name + "\n" + pwd);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
