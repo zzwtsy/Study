3498e6dbf41f74368f99d9b3e2f8aa5f9d9849b7
zzwtsy <m3226257319@gmail.com>
2022-11-20 16:29:28 +0800

    注册时判断用户名是否已注册

--- javaweb/hwk/day23/src/main/java/cn/acdt/hwk/day23/action/RegisterSvl.java
+++ javaweb/hwk/day23/src/main/java/cn/acdt/hwk/day23/action/RegisterSvl.java
@@ -1,16 +1,14 @@
 package cn.acdt.hwk.day23.action;
 
-import cn.acdt.hwk.day23.database.DataBaseHelper;
-import lombok.extern.log4j.Log4j2;
+import cn.acdt.hwk.day23.database.*;
+import lombok.extern.log4j.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.sql.SQLException;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+import java.util.*;
 
 /**
  * 注册svl
@@ -34,20 +32,32 @@ public class RegisterSvl extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Map<Object, Object>> checkUserNameList;
         HttpSession session = request.getSession();
         String name = request.getParameter("rusername");
         String password = request.getParameter("rpassword1");
-        String sql = "INSERT INTO `user` SET username = ?, password = ?";
+        String checkUserNameSql = "SELECT username FROM `user` WHERE username = ?";
         try {
-            if (DataBaseHelper.executeUpdate(sql, name, password) > 0) {
-                registerStatus = "注册成功";
-            } else {
-                registerStatus = "注册失败";
-            }
-            session.setAttribute("registerStatic", registerStatus);
+            checkUserNameList = DataBaseHelper.executeQuery(checkUserNameSql, name);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        //判断用户名是否已注册
+        if (checkUserNameList.size() > 0) {
+            registerStatus = "用户名已注册";
+        } else {
+            String sql = "INSERT INTO `user` SET username = ?, password = ?";
+            try {
+                if (DataBaseHelper.executeUpdate(sql, name, password) > 0) {
+                    registerStatus = "注册成功";
+                } else {
+                    registerStatus = "注册失败";
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        session.setAttribute("registerStatic", registerStatus);
         request.getRequestDispatcher("register.jsp").forward(request, response);
         log.info("用户注册账户");
     }
